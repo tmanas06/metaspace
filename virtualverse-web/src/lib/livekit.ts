99a92a1b3d74a8d953ed10c7228b455ba260c25e
tmanas06 <tmanas2004@gmail.com>
2026-08-15 11:04:55 +0530

    fixes ice shit

--- virtualverse-web/src/lib/livekit.ts
+++ virtualverse-web/src/lib/livekit.ts
@@ -264,16 +264,8 @@ class LiveKitManager {
         autoSubscribe: true,
         rtcConfig: {
           iceServers: [
-            { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] },
-            {
-              urls: [
-                "turn:openrelay.metered.ca:80",
-                "turn:openrelay.metered.ca:443",
-                "turn:openrelay.metered.ca:443?transport=tcp",
-              ],
-              username: "openrelayproject",
-              credential: "openrelayproject",
-            },
+            { urls: "stun:stun.l.google.com:19302" },
+            { urls: "stun:stun.cloudflare.com:3478" },
           ],
         },
       });
